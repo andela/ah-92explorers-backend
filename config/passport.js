01a0cd4f481ac854d07751adfcbac200e14ce034
Alpha Omondi Ogilo <alphaogilo@gmail.com>
2019-04-15 17:31:16 +0200

    Delete package-lock.json
    Package vulnerabilities detected. This can always be recreated on installation of packages defined in package.json file.
    
    chore(unit testing): setup unit testing
    
    - setup unit testing
    - ensure all back end functionality is tested
    
    [Maintains #166789985]

--- config/passport.js
+++ config/passport.js
@@ -3,11 +3,11 @@ const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 
-passport.use(
+module.exports = passport.use(
     new LocalStrategy(
         {
-            usernameField: "user[email]",
-            passwordField: "user[password]"
+            usernameField: "email",
+            passwordField: "password"
         },
         function(email, password, done) {
             User.findOne({ email: email })
