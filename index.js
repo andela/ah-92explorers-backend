a14d24906204fea6742efdc0893bf995ed9184f0
Celestin NIYONSABA <niyoceles3@gmail.com>
2019-06-27 09:55:58 +0200

    chore(set-up heroku): deploy application to heroku
    - setup heroku url
    - setup database to heroku
    
    [Maintains #166789984]

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-import fs from 'fs';
-import http from 'http';
-import path from 'path';
-import methods from 'methods';
-import express from 'express';
-import bodyParser from 'body-parser';
-import session from 'express-session';
-import cors from 'cors';
-import passport from 'passport';
-import errorhandler from 'errorhandler';
-import mongoose from 'mongoose';
-import swaggerUI from 'swagger-ui-express';
-import swagger from './swaggerSetUp/swaggerSetup';
-
-const isProduction = process.env.NODE_ENV === "production";
-const isTest = process.env.NODE_ENV === "test";
+import express from "express";
+import bodyParser from "body-parser";
+import session from "express-session";
+import cors from "cors";
+import swaggerUI from "swagger-ui-express";
+import swagger from "./swaggerSetUp/swaggerSetup";
+import welcomeRouter from "./routes/welcomeRouter";
 
 // Create global app object
 const app = express();
@@ -21,88 +12,56 @@ const app = express();
 app.use(cors());
 
 // Normal express config defaults
-app.use(require("morgan")("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(require("method-override")());
 app.use(express.static(__dirname + "/public"));
 
-app.get('/swagger.json', (req, res, next) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swagger.swaggerSpec);
+app.get("/swagger.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swagger.swaggerSpec);
 });
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swagger.swaggerSpec));
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swagger.swaggerSpec));
 
 app.use(
-    session({
-        secret: "authorshaven",
-        cookie: { maxAge: 60000 },
-        resave: false,
-        saveUninitialized: false
-    })
+  session({
+    secret: "authorshaven",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+  })
 );
-
-if (!isProduction) {
-    app.use(errorhandler());
-}
-
-if (isProduction) {
-    mongoose.connect(process.env.MONGODB_URI);
-}
-else if(isTest){
-  mongoose.connect("mongodb://localhost/dbTest");
-}
-else {
-    mongoose.connect("mongodb://localhost/conduit");
-    mongoose.set("debug", true);
-}
 
 require("./models/User");
 
 app.use(require("./routes"));
+app.use("/welcome", welcomeRouter);
 
 /// catch 404 and forward to error handler
 app.use(function(_req, _res, next) {
-    const err = new Error("Not Found");
-    err.status = 404;
-    next(err);
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (!isProduction) {
-    app.use(function(err, _req, res, _next) {
-        console.log(err.stack);
-
-        res.status(err.status || 500);
-
-        res.json({
-            errors: {
-                message: err.message,
-                error: err
-            }
-        });
-    });
-}
-
-//production error handler
 // no stacktraces leaked to user
-app.use(function(err, _req, res, _next) {
-    res.status(err.status || 500);
-    res.json({
-        errors: {
-            message: err.message,
-            error: {}
-        }
-    });
+app.use((err, res) => {
+  if (err) {
+    return res.status(err.status || 500);
+  }
+
+  return res.json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  });
 });
 
 // finally, let's start our server...
 const server = app.listen(process.env.PORT || 3000, function() {
-    console.log("Listening on port " + server.address().port);
+  console.log("Listening on port " + server.address().port);
 });
 
 export default app;
