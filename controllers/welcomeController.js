3bcac0269094637669a13c319940a8c8e0dd5da3
Celestin NIYONSABA <30776949+niyoceles@users.noreply.github.com>
2019-06-28 14:32:03 +0200

    bug(jsonify-response): convert response to json
    - change response to json
    - rename heroku app to ah-92explorers-api
    
    [Finishes #166954780]

--- controllers/welcomeController.js
+++ controllers/welcomeController.js
@@ -4,12 +4,9 @@ dotenv.config();
 
 class WelcomeController {
   static welcome(req, res) {
-    res
-      .status(200)
-      .send(
-        "Welcome to Authors Haven - A Social platform for the creative at heart."
-      );
-    res.status(400).send("Server Error");
+    res.status(200).json({
+      msg: "Welcome to Authors Haven API, a #92Explorers Product."
+    });
   }
 }
 
