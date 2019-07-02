c6eee1459080af377a0ba7e515f12cd9de60b8fb
Helm Zero <41841554+kodek-sleuth@users.noreply.github.com>
2019-07-02 13:31:59 +0200

    feat(input-validation): validate user input on signup
    - add user input validation
    - ensure users receive descriptive signup validation errors
    
    [Finishes #166789991]

--- models/index.js
+++ models/index.js
@@ -5,6 +5,7 @@ import configs from '../config'; // Importing configuration file
 
 require('dotenv').config(); // Enabling the use of the env variables
 
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development'; // We use either NODE_ENV/ 'development'
 const config = configs[env]; // Config variable takes in our new configuration
