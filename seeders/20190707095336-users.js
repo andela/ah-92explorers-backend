5d0df95a9a7395437cc8cc8f2541b8ff739f3bbe
Helm Zero <41841554+kodek-sleuth@users.noreply.github.com>
2019-07-09 19:22:54 +0200

    feature(create-crud-superadmin): enable superadmin to perform crud operations on users
    - create class to implement crud operations
    - store user data in database
    - write tests for all crud operations introduced
    
    [Delivers #166790016]

---
+++ seeders/20190707095336-users.js
@@ -0,0 +1,24 @@
+import uuid from 'uuidv4';
+
+  export const up = (queryInterface, Sequelize) => queryInterface.bulkInsert('users', [
+  {
+    id: uuid(),
+    email: 'johnnie@gmail.com',
+    username: 'jhonnie',
+    password: 'Password12$',
+    accessLevel: 2
+  },
+  {
+    id: uuid(),
+    email: 'peter@gmail.com',
+    username: 'peterJ',
+    password: 'Password12$',
+    accessLevel: 1
+  }
+
+], {})
+
+  const down = (queryInterface, Sequelize) => {
+      return queryInterface.bulkDelete('users', null, {});
+  }
+
