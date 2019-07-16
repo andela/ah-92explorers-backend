80437ae59cc316fe48b4671fb6a4fec475801fcf
Herve Nkurikiyimfura <nkuliherve@gmail.com>
2019-07-16 10:23:52 +0200

    feat(reset-password): reset password via email
    - add nodemailer
    - write send mail functionalities
    - write reset password functionalities
    - write tests
    
    [Delivers #166789994]

---
+++ helpers/mailer.js
@@ -0,0 +1,59 @@
+import nodemailer from 'nodemailer';
+import { email } from '../config/index';
+import Templates from './emailTemplates';
+
+const { emailTemplates } = Templates;
+/**
+ * Class module to send email
+ * @exports
+ * @class
+ */
+class Mailer {
+  /**
+   * Initialize user and pass
+   * @constructor
+   * @param {string} _userEmail - User email
+   */
+  constructor(_userEmail) {
+    this.userEmail = _userEmail;
+    this.senderEmail = email.user;
+    this.senderPass = email.pass;
+  }
+
+  /**
+ * Adds a token.
+ * @param {string} token token
+ * @param {string} template email.
+ * @returns {object} response.
+ */
+  async addTokenToEmail(token, template) {
+    const mailOptions = emailTemplates[template];
+    mailOptions.from = this.senderEmail;
+    mailOptions.to = this.userEmail;
+    const addToken = mailOptions.html.replace('$token', token);
+    mailOptions.html = addToken;
+    try {
+      const response = await this.sendEmail(mailOptions);
+      return response;
+    } catch (error) {
+      return `${error}`;
+    }
+  }
+
+  /**
+   * Send email
+   * @param {Object} mailOptions - Email template
+   * @returns {object} response.
+   */
+  async sendEmail(mailOptions) {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: this.senderEmail,
+        pass: this.senderPass
+      }
+    });
+    transporter.sendMail(mailOptions);
+  }
+}
+export default Mailer;
