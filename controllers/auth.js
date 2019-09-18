0eb623fe5728a6a615a39d4868d8e49c0adc65eb
Celestin NIYONSABA <niyoceles3@gmail.com>
2019-09-18 15:27:54 +0200

    bug(fix-signin-verified-account): ensure signin with verified account - enable only verified account to signin
    [fixes #168574664]

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,6 +9,7 @@ const { users, sequelize } = db;
 
 dotenv.config();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const { APP_URL_BACKEND, FRONT_END_URL } = process.env;
 
 export const signup = async (req, res) => {
   const {
@@ -17,15 +18,18 @@ export const signup = async (req, res) => {
   const hashedPassword = Auth.hashPassword(password);
   const transaction = await sequelize.transaction({ autocommit: false });
   try {
-    const newUser = await users.create({
-      username,
-      firstName: firstname,
-      lastName: lastname,
-      email,
-      password: hashedPassword,
-    }, {
-      transaction,
-    });
+    const newUser = await users.create(
+      {
+        username,
+        firstName: firstname,
+        lastName: lastname,
+        email,
+        password: hashedPassword
+      },
+      {
+        transaction
+      }
+    );
     await transaction.commit();
     if (newUser) {
       const token = Auth.genToken(username, email, newUser.id);
@@ -36,11 +40,11 @@ export const signup = async (req, res) => {
         html: `<div style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;padding:35px;">
           <h1 style="color: #444;">Authors Haven</h1>
           <p style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left">Welcome ${username},<br> We are happy to have you onboard. Please verify your mail to enjoy premium access.<br> Click the blue button below to verify your account.</p>
-          <p><a style="background-color: #3097d1; border: 2px solid #3097d1; padding: 8px; color: #fff; font-size: 16px; text-decoration: none;cursor: pointer;" href="${process.env.HOST}/api/users/verify/${token}">Verify Account</a>
+          <p><a style="background-color: #3097d1; border: 2px solid #3097d1; padding: 8px; color: #fff; font-size: 16px; text-decoration: none;cursor: pointer;" href="${APP_URL_BACKEND}/api/users/verify/${token}">Verify Account</a>
           </a></p>
           <p style="color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left">Thank you for using our application!</p>
           <p style="color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;">Regards,<br>Authors Haven</p>
-          </div>`,
+          </div>`
       };
       sgMail.send(msg);
       return res.status(201).json({
@@ -48,8 +52,8 @@ export const signup = async (req, res) => {
         user: {
           token: Auth.genToken(username, email, newUser.id),
           username: newUser.username,
-          email: newUser.email,
-        },
+          email: newUser.email
+        }
       });
     }
   } catch (ex) {
@@ -63,22 +67,31 @@ export const signin = async (req, res) => {
   const transaction = await sequelize.transaction();
   try {
     const user = await users.findOne({ where: { email: req.body.email } }, { transaction });
+    const verifiedUser = await users.findOne(
+      { where: { email: req.body.email, isVerified: true } },
+      { transaction }
+    );
     await transaction.commit();
+
     if (!user) {
       return res.status(404).json({ error: 'user not found' });
     }
-    const passBool = Auth.comparePassword(password, user.password);
-    const { username } = user;
+
+    if (!verifiedUser) {
+      return res.status(404).json({ error: 'please verify your account ' });
+    }
+    const passBool = Auth.comparePassword(password, verifiedUser.password);
+    const { username } = verifiedUser;
     if (passBool) {
       return res.status(200).json({
         message: 'logged in',
         user: {
-          id: user.id,
-          token: Auth.genToken(username, email, user.id),
+          id: verifiedUser.id,
+          token: Auth.genToken(username, email, verifiedUser.id),
           username,
-          email: user.email,
-          image: user.image,
-        },
+          email: verifiedUser.email,
+          image: verifiedUser.image
+        }
       });
     }
     return res.status(401).json({ error: 'wrong username or password' });
@@ -92,27 +105,24 @@ export const verifyUser = async (req, res) => {
   const { token } = req.params;
   const decodedToken = jwt.verify(token, process.env.SECRET);
   try {
-    const user = await users.findOne(decodedToken.id);
+    const user = await users.findOne({ where: { id: decodedToken.id }});
     if (!user) {
       return res.status(404).json({
         status: res.statusCode,
-        error: 'user not registered',
+        error: 'user not registered'
       });
     }
     const updatedUser = await users.update(
       { isVerified: true },
-      { where: { email: decodedToken.email } },
+      { where: { email: decodedToken.email } }
     );
     if (updatedUser) {
-      return res.status(200).json({
-        status: 200,
-        message: 'user has been verified',
-      });
+      return res.redirect(`${FRONT_END_URL}/login`);
     }
   } catch (error) {
     return res.status(500).json({
       status: 500,
-      error: 'something went wrong',
+      error: 'something went wrong'
     });
   }
 };
